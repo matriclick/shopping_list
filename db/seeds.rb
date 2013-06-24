7bb005513a7328b217a7d9cc141c1ab2c03d5b70
hhanckes <hhanckes@gmail.com>
2013-06-24 16:36:11 -0400

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "--> Dress Types:"
+aux = []
+aux << (Tag.find_by_name("Deportista") || Tag.create(:name => 'Deportista', :image_name => "deportista.jpg"))
+aux << (Tag.find_by_name("Vegetariano") || Tag.create(:name => 'Vegetariano', :image_name => "vegetariano.jpg"))
+aux << (Tag.find_by_name("Balanceado") || Tag.create(:name => 'Balanceado', :image_name => "balanceado.jpg"))
+aux << (Tag.find_by_name("Bajas Calorías") || Tag.create(:name => 'Bajas Calorías', :image_name => "bajas-calorias.jpg"))
+aux.each { |x| puts x.name}
+puts "\n"
