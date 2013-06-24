d13d90467c38c636dd7450c2f4f473b4795ad8ed
hhanckes <hhanckes@gmail.com>
2013-06-24 16:39:06 -0400

    coding: utf-8

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# coding: utf-8
+
 puts "--> Dress Types:"
 aux = []
 aux << (Tag.find_by_name("Deportista") || Tag.create(:name => 'Deportista', :image_name => "deportista.jpg"))
