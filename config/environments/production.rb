3dd815d5ec6c88d4acf595978b98afdbb362a1de
hhanckes <hhanckes@gmail.com>
2013-06-24 15:53:41 -0400

    arreglo a production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Matri::Application.configure do
+ShoppingCart::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
